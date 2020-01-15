b3ed3124ba4539d90a6284ecb17fe86246c05448
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-01-14 20:21:53 -0800

    edited for server

---
+++ server.js
@@ -0,0 +1,52 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+// create table array 
+var table =[];
+
+// create waitlist array
+
+var wait = [];
+
+// routes
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+app.get("/reservations", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservations.html"));
+  });
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+
+app.get("/api/table", function(req, res) {
+   return res.json(table);
+  });
+app.get("/api/wait", function(req, res) {
+    return res.json(wait);
+  });
+
+  // post data for reservations
+
+
+
+
+
+
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
